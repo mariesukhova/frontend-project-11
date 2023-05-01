826197084a198d07037d734a8606b542039370fd
Maria Sukhova <mariesukhova@gmail.com>
2023-05-01 17:37:46 +0300

    [step-3] Remove validator function as separate file

--- src/validator.js
+++
@@ -1,7 +0,0 @@
-import * as yup from 'yup';
-
-export default function validator(target, list) {
-  const schema = yup.string().required().url().notOneOf(list);
-
-  return schema.isValid(target);
-}
