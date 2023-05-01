cfb3b953c0ab0968d8ac3deb1affdcecb1271d88
Maria Sukhova <mariesukhova@gmail.com>
2023-05-01 17:35:33 +0300

    [step-3] Add ru-en locales

---
+++ src/locales/en.js
@@ -0,0 +1,22 @@
+export default {
+  translation: {
+    interface: {
+      headline: 'RSS aggregator',
+      description: 'Start reading RSS today! It`s easy, it`s beautiful.',
+      addButton: 'Add',
+      hint: 'Example: https://ru.hexlet.io/lessons.rss',
+      text: 'RSS link:',
+      posts: 'Posts',
+      feeds: 'Feeds',
+      previewButton: 'Preview',
+    },
+    errors: {
+      empty: "Shouldn't be empty",
+      alreadyExist: 'RSS already exists',
+      notValid: 'Should be valid URL',
+      notRss: 'RSS not valid',
+      network: 'Network error',
+    },
+    success: 'RSS loaded successfully',
+  },
+};
---
+++ src/locales/ru.js
@@ -0,0 +1,22 @@
+export default {
+  translation: {
+    interface: {
+      headline: 'RSS агрегатор',
+      description: 'Начните читать RSS сегодня! Это легко, это красиво.',
+      addButton: 'Добавить',
+      hint: 'Пример: https://ru.hexlet.io/lessons.rss',
+      text: 'Ссылка RSS',
+      posts: 'Посты',
+      feeds: 'Фиды',
+      previewButton: 'Просмотр',
+    },
+    error: {
+      empty: 'Не должно быть пустым',
+      alreadyExists: 'RSS уже существует',
+      notValid: 'Ссылка должна быть валидным URL',
+      notRss: 'Ресурс не содержит валидный RSS',
+      network: 'Ошибка сети',
+    },
+    success: 'RSS успешно загружен',
+  },
+};
