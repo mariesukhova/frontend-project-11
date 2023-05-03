a4d4ba94f3417ee478b3dc43e84719c89e99096d
Maria Sukhova <mariesukhova@gmail.com>
2023-05-03 17:47:39 +0300

    [step-4] Add loadData and handleError functions

--- src/init.js
+++ src/init.js
@@ -1,7 +1,22 @@
+/* eslint-disable no-param-reassign */
 import i18next from 'i18next';
 import * as yup from 'yup';
+import axios from 'axios';
+import parser from './parser.js';
 import generateWatchedState from './view.js';
 import resources from './locales/index.js';
+
+function loadData(url) {
+  const proxyUrl = `https://allorigins.hexlet.app/get?disableCache=true&url=${url}`;
+  return axios.get(proxyUrl)
+    .then((response) => {
+      if (response.data.status.http_code !== 200) throw new Error('tooManyRequests');
+      return parser(response.data.contents);
+    })
+    .catch((e) => {
+      throw e;
+    });
+}
 
 const app = async () => {
   const i18nInstance = i18next.createInstance();
@@ -25,31 +40,58 @@ const app = async () => {
   const elements = {
     textInput: document.querySelector('.rss-form #url-input'),
     rssForm: document.querySelector('.rss-form'),
+    posts: document.querySelector('.posts'),
+    feeds: document.querySelector('.feeds'),
   };
 
   const state = {
     status: '',
     error: '',
     links: [],
-    fids: [],
+    feeds: [],
     posts: [],
   };
 
   const watchedState = generateWatchedState(state, elements, i18nInstance);
+
+  function handleError(error) {
+    switch (error.name) {
+      case 'ValidationError':
+        watchedState.error = error.message;
+        watchedState.status = 'in-valid';
+        break;
+      case 'AxiosError':
+        watchedState.error = error.message;
+        watchedState.status = 'failed';
+        break;
+      case 'Error':
+        watchedState.error = error.message;
+        watchedState.status = 'failed';
+        break;
+      default:
+        watchedState.error = 'unknownError';
+        watchedState.status = 'failed';
+    }
+  }
 
   elements.rssForm.addEventListener('submit', (e) => {
     e.preventDefault();
     const formData = e.target.url.value;
     const schema = yup.string().required().url().notOneOf(watchedState.links);
     schema.validate(formData)
-      .then(() => {
-        watchedState.status = '';
-        watchedState.status = 'valid';
-        watchedState.links.push(formData);
-      })
+      .then(() => loadData(formData, watchedState)
+        .then((data) => {
+          watchedState.status = '';
+          watchedState.status = 'valid';
+          watchedState.links.push(formData);
+          watchedState.feeds.unshift(data.feeds);
+          data.posts.forEach((post) => {
+            watchedState.posts.push(post);
+          });
+        }))
       .catch((error) => {
-        watchedState.error = error;
-        watchedState.status = 'in-valid';
+        console.log(error);
+        handleError(error);
       });
   });
 };
